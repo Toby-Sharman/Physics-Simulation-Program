36cabbc2b111b51ca58d80a99421f9c1ff306f57
trjs <tobiassharman@gmail.com>
2025-09-18 16:41:21 +0100

    Added << operator for quantity printing and unit return function

--- core/maths/utilities/quantity.h
+++ core/maths/utilities/quantity.h
@@ -6,12 +6,20 @@
 #define QUANTITY_H
 
 #include <string>
+#include <iostream>
 
 // Quantity struct
 struct Quantity {
     double value;
     std::string unit;
     [[nodiscard]] double asDouble() const { return value; }
+    [[nodiscard]] std::string asUnit() const { return unit; }
 };
+
+// Non-member operator<< TODO: Format library support
+inline std::ostream& operator<<(std::ostream& os, const Quantity& q) {
+    os << q.value << " " << q.unit;
+    return os;
+}
 
 #endif //QUANTITY_H
