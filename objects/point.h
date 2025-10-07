0ffa6378f7dfa69ce9aa18fe89340af753b9fbf8
trjs <tobiassharman@gmail.com>
2025-10-07 15:51:53 +0100

    Temp comment out to fix changes in object describe self method

--- objects/point.h
+++ objects/point.h
@@ -1,26 +1,26 @@
+// //
+// // Created by Tobias Sharman on 08/09/2025.
+// //
 //
-// Created by Tobias Sharman on 08/09/2025.
+// #ifndef PHYSICS_SIMULATION_PROGRAM_POINT_H
+// #define PHYSICS_SIMULATION_PROGRAM_POINT_H
 //
-
-#ifndef PHYSICS_SIMULATION_PROGRAM_POINT_H
-#define PHYSICS_SIMULATION_PROGRAM_POINT_H
-
-#include "vector.h"
-
-#include "object.h"
-
-class Point : public Object {
-    public:
-
-    // void set_size_from_params(const Vector<1>& dimensions);
-    // bool contains_point(const Vector<3>& world_point) const override;
-
-    protected:
-    std::string getSizeDescription() const override;
-
-    private:
-    Vector<1> m_size = Vector<1>{0};
-    // double bounding_radius_{0.0f};
-};
-
-#endif //PHYSICS_SIMULATION_PROGRAM_POINT_H
+// #include "vector.h"
+//
+// #include "object.h"
+//
+// class Point : public Object {
+//     public:
+//
+//     // void set_size_from_params(const Vector<1>& dimensions);
+//     // bool contains_point(const Vector<3>& world_point) const override;
+//
+//     protected:
+//     std::string getSizeDescription() const override;
+//
+//     private:
+//     Vector<1> m_size = Vector<1>{0};
+//     // double bounding_radius_{0.0f};
+// };
+//
+// #endif //PHYSICS_SIMULATION_PROGRAM_POINT_H
