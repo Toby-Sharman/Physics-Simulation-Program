79545e43b9883b879a2dcd7fc722451e8eef4b30
trjs <tobiassharman@gmail.com>
2025-10-13 14:15:25 +0100

    Fixed handling for ct field of 4 position

--- simulation/particle-movement/particle_movement.cpp
+++ simulation/particle-movement/particle_movement.cpp
@@ -8,28 +8,33 @@
 
 void moveParticle(Particle& particle, const Quantity& dt) {
     auto& momentum4Vector = particle.getMomentum();
-    const auto& energy = momentum4Vector[0];
     const auto& momentum = *reinterpret_cast<Vector<3> const*>(&momentum4Vector[1]);
+
     auto& initialPosition4Vector = particle.getPosition();
     const auto& initialTime = initialPosition4Vector[0];
     const auto& initialPosition = *reinterpret_cast<Vector<3> const*>(&initialPosition4Vector[1]);
+
     Vector<4> finalPosition;
 
+     const auto lightDistance = dt * quantityTable().at("c");
+
     if (particle.isMassless()) {
-        const auto displacement = momentum.unitVector() * dt * quantityTable().at("c");
+        const auto displacement = momentum.unitVector() * lightDistance;
 
         for (size_t i = 0; i < 3; ++i) {
             finalPosition[i + 1] = initialPosition[i] + displacement[i];
         }
-        finalPosition[0] = initialTime + dt;
+        finalPosition[0] = initialTime + lightDistance;
         particle.setPosition(finalPosition);
     } else { // massive particle
+        const auto& energy = momentum4Vector[0];
+
         const auto displacement = momentum / energy * dt * quantityTable().at("c");
 
         for (size_t i = 0; i < 3; ++i) {
             finalPosition[i + 1] = initialPosition[i] + displacement[i];
         }
-        finalPosition[0] = initialTime + dt;
+        finalPosition[0] = initialTime + lightDistance;
         particle.setPosition(finalPosition);
     }
 }
