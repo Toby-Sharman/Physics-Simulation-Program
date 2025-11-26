6eef5fc25a310b19e6e5cad569e9ad6cfeed0589
trjs <tobiassharman@gmail.com>
2025-11-26 15:26:30 +0000

    Make field_solver more inline with the rest of the project

--- physics/fields/field_solver.cpp
+++ physics/fields/field_solver.cpp
@@ -1,21 +1,32 @@
 //
-// Created by Tobias Sharman on 10/09/2025.
+// Physics Simulation Program
+// File: field_solver.cpp
+// Created by Tobias Sharman on 10/09/2025
+//
+// Description:
+//   - Implementation of field_solver.h
+//
+// Copyright (c) 2025, Tobias Sharman
+// Licensed under a Non-Commercial License. See LICENSE file for details
 //
 
-#include "field_solver.h"
-#include "globals.h"
-#include "material_database.h"
+#include "Physics/fields/field_solver.h"
 
-auto globalB = Vector<3>{{0.0, 0.0, 1.0},"T"};
-auto globalH = globalB / Globals::Constant::Physics::mu0;
+#include "core/globals.h"
+#include "databases/material-data/material_database.h"
+#include "objects/object_manager.h"
 
-Vector<3> getFieldAtPoint(const Vector<3>& point, const std::shared_ptr<Object>& root) {
+auto g_BFieldStrength = Vector<3>{{0.0, 0.0, 1.0},"T"};
+auto g_HFieldStrength = g_BFieldStrength / Globals::Constant::Physics::mu0;
+
+Vector<3> getFieldAtPoint(const Vector<3>& point) {
+    const auto root = g_objectManager.getActiveWorld();
     if (!root) {
-        return globalB;
+        return g_BFieldStrength;
     }
     const auto obj = root->findObjectContaining(point);
     if (!obj) {
-        return globalB;
+        return g_BFieldStrength;
     }
-    return globalB * materialDatabase.getRelativePermeability(obj->getMaterial());
+    return g_BFieldStrength * g_materialDatabase.getRelativePermeability(obj->getMaterial());
 }
--- physics/fields/field_solver.h
+++ physics/fields/field_solver.h
@@ -1,16 +1,25 @@
 //
-// Created by Tobias Sharman on 10/09/2025.
+// Physics Simulation Program
+// File: field_solver.h
+// Created by Tobias Sharman on 10/09/2025
 //
+// Description:
+//   - Helper for getting magnetic field at a point - currently very simply unrealistic implementation
+//
+// Copyright (c) 2025, Tobias Sharman
+// Licensed under a Non-Commercial License. See LICENSE file for details
+//
+
+// TODO: Implement more realistic fields and base field strength
 
 #ifndef PHYSICS_SIMULATION_PROGRAM_FIELD_SOLVER_H
 #define PHYSICS_SIMULATION_PROGRAM_FIELD_SOLVER_H
 
-#include "vector.h"
+#include "core/linear-algebra/vector.h"
 
-#include "object.h"
+extern Vector<3> g_BFieldStrength;
+extern Vector<3> g_HFieldStrength;
 
-extern Vector<3> globalB;
-
-Vector<3> getFieldAtPoint(const Vector<3>& point, const std::shared_ptr<Object>& root);
+Vector<3> getFieldAtPoint(const Vector<3>& point);
 
 #endif //PHYSICS_SIMULATION_PROGRAM_FIELD_SOLVER_H
